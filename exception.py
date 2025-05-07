5c0d5835bd259767a643b4e715d5e4865f0509ef
parth34patil <parrthhp67@gmail.com>
2025-05-07 19:41:22 +0530

    add comment with code

--- exception.py
+++ exception.py
@@ -1,9 +1,13 @@
+# Try block to catch any exceptions that might occur
 try:
-   enter_file_name = input("Enter file name = ")
-   with open(enter_file_name,"r") as file:
-       print(file.read())
+    # Ask the user to enter the name of the file
+    enter_file_name = input("Enter file name = ")
 
+    # Open the file in read mode and assign it to 'file' using 'with' (auto-closes file)
+    with open(enter_file_name, "r") as file:
+        # Read and print the contents of the file
+        print(file.read())
+
+# Handle the case where the file doesn't exist
 except FileNotFoundError:
-    print("this file is not availabel")
-
-
+    print("This file is not available")
